182a1d1246395c51f041d7740e451ddf0627e7ef
MrSenan <101907742+MrSenan@users.noreply.github.com>
2022-12-20 13:43:25 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-
-token = TOKEN
-
-
-genres = {
-    'Ужасы': 'https://newdeaf.lol/genre/%D1%83%D0%B6%D0%B0%D1%81%D1%8B/',
-    'Триллер': 'https://newdeaf.lol/genre/%D1%82%D1%80%D0%B8%D0%BB%D0%BB%D0%B5%D1%80/'
-}
